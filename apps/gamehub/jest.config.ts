f22e8504b5e6d823d34bad273d11452cde4e7748
Duc Le <minhduc.le@mail.utoronto.ca>
2024-01-25 19:51:30 -0500

    Still initial commit

--- apps/gamehub/jest.config.ts
+++ apps/gamehub/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'bug-buster',
+  displayName: 'gamehub',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/bug-buster',
+  coverageDirectory: '../../coverage/apps/gamehub',
 };
