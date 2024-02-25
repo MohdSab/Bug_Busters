ce4aaed0f644426447dc4bd57e6a36e0b5fc2bbb
Smartie22 <meikymeily2003@gmail.com>
2024-02-25 15:31:57 -0500

    added TttRepo to providers

--- apps/ttt-service/src/app/app.module.ts
+++ apps/ttt-service/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicTacToeGameLogic } from './ttt-game-logic-gateway';
+import { TttRepo } from './ttt.repo';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, TicTacToeGameLogic],
+  providers: [AppService, TicTacToeGameLogic, TttRepo],
 })
 export class AppModule {}
