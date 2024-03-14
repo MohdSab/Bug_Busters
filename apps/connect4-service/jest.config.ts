67b6408211ec0390d7e2773c43673b12005dbece
clarks03 <stephen.clark@mail.utoronto.ca>
2024-03-14 19:45:46 -0400

    [C4-BE] first commit, created nest project

---
+++ apps/connect4-service/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'connect4-service',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/connect4-service',
+};
