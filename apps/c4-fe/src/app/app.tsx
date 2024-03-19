c387988a9d5257f9b3f98e68c28df9e51391970e
Duc Le <minhduc.le@mail.utoronto.ca>
2024-03-19 00:40:44 -0400

    [C4]: Create simple c4 frontend

--- apps/c4-fe/src/app/app.tsx
+++ apps/c4-fe/src/app/app.tsx
@@ -1,51 +1,88 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
+function EmptyCell() {
+  return <div className={styles.emptyCell} />;
+}
+
+function P1Cell() {
+  return <div className={styles.p1Cell} />;
+}
+
+function P2Cell() {
+  return <div className={styles.p2Cell} />;
+}
+
+function Butt({ onClick }: { onClick: () => void }) {
+  return <div onClick={onClick}>Butt</div>;
+}
+
+const initalState: number[][] = Array(6).fill(Array(7).fill(0));
+
 export function App() {
+  const [board, setBoard] = useState(initalState);
+  const [isP1, setIsP1] = useState(true);
+
+  const [something, fkoff] = useState<number | null>(null);
+
+  // Read about useEffect
+
+  // Read ttt for how to use Auth service and websocket context
+
+  // Design the action flow
+
+  // console.log
+
+  const handleClick = (col: number) => {
+    if (isP1) {
+      setIsP1(false);
+
+      setIsP1((old) => !old);
+
+      setBoard((old) => {
+        const newBoard = [...old];
+        newBoard[3][col] = isP1 ? 1 : 2;
+        return newBoard;
+      });
+    }
+
+    console.log('P' + isP1 + ' Clicked on row ' + col);
+    setIsP1((old) => !old);
+
+    // Animations and stuff
+  };
+
   return (
     <div>
-      <NxWelcome title="c4-fe" />
+      <h1 style={{ textAlign: 'center' }}>Connect 4</h1>
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
+      <div id={styles.board}>
+        <div id={styles.buttons}>
+          {Array(7)
+            .fill(0)
+            .map((_, i) => (
+              <Butt key={`button-${i}`} onClick={() => handleClick(i)} />
+            ))}
+        </div>
+        {board.map((row, i) => (
+          <div className={styles.row} key={`row-${i}`}>
+            {row.map((cell, j) => (
+              <>
+                {cell === 0 ? (
+                  <EmptyCell key={`cell-${i + j}`} />
+                ) : cell === 1 ? (
+                  <P1Cell key={`cell-${i + j}`} />
+                ) : (
+                  <P2Cell key={`cell-${i + j}`} />
+                )}
+              </>
+            ))}
+          </div>
+        ))}
       </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
     </div>
   );
 }
