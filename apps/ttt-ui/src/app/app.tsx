c4210b11385bba5ff576d42324cff695361e13e6
Duc Le <minhduc.le@mail.utoronto.ca>
2024-03-23 18:17:56 -0400

    [TTT]: Dockerize ttt-ui

--- apps/ttt-ui/src/app/app.tsx
+++ apps/ttt-ui/src/app/app.tsx
@@ -5,17 +5,25 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import TicTacToe from './components/game';
 import { TttLandingPage } from './pages/ttt-landing-page';
 import { ErrorPage } from './pages/error-page';
-import SignIn from '../../../gamehub/src/app/pages/SignIn';
-import SignUp from '../../../gamehub/src/app/pages/SignUp';
+// import SignIn from '../../../gamehub/src/app/pages/SignIn';
+// import SignUp from '../../../gamehub/src/app/pages/SignUp';
 import { AccountProvider } from '@bb/auth-hook-lib';
 import { WebsocketProvider } from './contexts/websocket-context';
 
+function SignUp() {
+  return <div>Sign up</div>;
+}
+
+function SignIn() {
+  return <div>Sign in</div>;
+}
+
 const router = createBrowserRouter([
-  { path: '/', Component: TttLandingPage  },
+  { path: '/', Component: TttLandingPage },
   { path: '/game/:id', element: <TicTacToe /> },
   { path: '/error', element: <ErrorPage /> },
-  { path: '/signin', element: <SignIn />},
-  { path: '/signup', element: <SignUp />},
+  { path: '/signin', element: <SignIn /> },
+  { path: '/signup', element: <SignUp /> },
 ]);
 
 export function App() {
