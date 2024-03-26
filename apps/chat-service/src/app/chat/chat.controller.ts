36f2ab748b140c7c72fadd47ca129e7f1f5f88bf
Sepiidd <sebastianpedrucochoi@hotmail.com>
2024-03-25 20:00:39 -0400

    [CHAT] - docker related stuff and attempted apigateway registration

--- apps/chat-service/src/app/chat/chat.controller.ts
+++ apps/chat-service/src/app/chat/chat.controller.ts
@@ -5,16 +5,37 @@ import {
     Res,
 } from "@nestjs/common";
 import { ChatService } from "./chat.service"; // to store/check for created rooms
+import { Gateway } from "@bb/gateway-lib";
 
 type CreateRoomDTO = {
     game: string,
     gameRoomID: number
 }
 
+const key = process.env.CHAT_SERVICE_KEY || 'chat-service';
+const port = Number(process.env.CHAT_SERVICE_PORT) || 4000;
+
+//idk wtf im doing here
+const GTWY_HOST = process.env.GATEWAY_HOST || 'localhost';
+const GTWY_PORT = process.env.GATEWAY_PORT || '3000'; 
+
 @Controller('chat')
 export class ChatController{
     
-    constructor(private chatService: ChatService) {}
+    constructor(private chatService: ChatService) {
+        //register this controller on the apigateway
+        
+        /*
+        new Gateway(`${process.env.GATEWAY_HOST}:${process.env.GATEWAY_PORT}`)
+        .RegisterService({
+            key: key,
+            port: port
+        }).then((route) => {
+            console.log('CHAT SERVICE registered with key', route.key, 'on endpoint', route.endpoint);
+        }).catch(console.error);
+        */
+
+    }
 
     @Post()
     reserveRoom(@Res() response, @Body() data:CreateRoomDTO): string{
